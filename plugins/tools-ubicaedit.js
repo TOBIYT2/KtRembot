28fdc928336a22e6fdb7dccef188707dd0a8c4d4
Tobiprueba <virtualvirtuales538@gmail.com>
2025-06-11 23:25:34 -0600

    Add files via upload

---
+++ plugins/tools-ubicaedit.js
@@ -0,0 +1,31 @@
+let handler = async (m, { text, conn }) => {
+  if (!text || !text.includes('.')) {
+    return m.reply('😭 Formato incorrecto.\nUsa: .ubicacion nombre.2');
+  }
+
+  let [nameText, nameRepeatStr] = text.split('.');
+  let nameRepeat = parseInt(nameRepeatStr);
+
+  nameText = nameText?.trim() || 'Japón';
+  nameRepeat = isNaN(nameRepeat) ? 1 : nameRepeat; // Sin límite
+
+  const finalName = nameText.repeat(nameRepeat);
+  const fixedAddress = '༺⃢🔥𝑇𝑂𝐵𝐼🔥⃢༻ ²⁰²⁴';
+
+  for (let i = 0; i < 5; i++) {
+    await conn.sendMessage(m.chat, {
+      location: {
+        degreesLatitude: 35.6895,
+        degreesLongitude: 139.6917,
+        name: finalName,
+        address: fixedAddress
+      }
+    }, { quoted: m });
+  }
+};
+
+handler.help = ['ubicacion2 <nombre>.n'];
+handler.tags = ['tools', 'fun'];
+handler.command = ['ubicacion2', 'ubicafake', 'mapafake'];
+
+export default handler;
