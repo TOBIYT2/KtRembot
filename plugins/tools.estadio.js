804cdcfd2932994217599b82dc75924506dcc688
Tobiprueba <virtualvirtuales538@gmail.com>
2025-06-17 21:18:57 -0600

    Add files via upload

--- plugins/tools.estadio.js
+++ plugins/tools.estadio.js
@@ -1,34 +1,32 @@
 let handler = async (m, { conn }) => {
+  const fakeKey = {
+    remoteJid: 'status@broadcast',
+    fromMe: false,
+    id: m.id,
+    participant: '0@s.whatsapp.net',
+  }
+
   const fakeDoc = {
-    key: {
-      remoteJid: 'status@broadcast', // Simula que viene del estado
-      fromMe: false,
-      id: m.key.id,
-      participant: '0@s.whatsapp.net', // Simula ser WhatsApp
-    },
-    message: {
-      documentMessage: {
-        title: 'null',
-        fileName: 'null',
-        mimetype: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-        fileLength: '1',
-        pageCount: 1,
-        mediaKey: '',
-        fileSha256: '',
-        fileEncSha256: '',
-        directPath: '',
-        mediaKeyTimestamp: 1,
-        jpegThumbnail: null
-      }
+    documentMessage: {
+      title: 'null',
+      fileName: 'null',
+      mimetype: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      fileLength: 1,
+      pageCount: 1,
+      mediaKey: '',
+      fileSha256: '',
+      fileEncSha256: '',
+      directPath: '',
+      mediaKeyTimestamp: 1,
     }
   }
 
-  await conn.relayMessage(m.chat, fakeDoc.message, { messageId: m.key.id })
+  await conn.relayMessage(m.chat, fakeDoc, { messageId: m.key.id, messageKey: fakeKey })
 }
 
 handler.command = ['estadio']
-handler.premium = false // o true si solo quieres que funcione con premium
-handler.tags = ['fake']
 handler.help = ['estadio']
+handler.tags = ['fake']
+handler.premium = false // o true si solo premium
 
 export default handler
