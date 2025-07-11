84230f6692cb52fdb548f4d62013c0dbbcc07844
Tobiprueba <virtualvirtuales538@gmail.com>
2025-07-10 23:03:26 -0600

    Add files via upload

--- plugins/tools-ios3.js
+++ plugins/tools-ios3.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
 
-const FILE_PATH = './mensajes_multi.json';
+const FILE_PATH = './mensajes_multi-3.json';
 
 let handler = async (m, { conn }) => {
   try {
